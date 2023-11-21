63a99663f8cf117da4087e9b729d85f3c7036c4b
Nonnorso <r.philippot@live.fr>
2023-11-21 11:59:23 +0100

    gestion du style header en desktop et reponsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,10 +6,11 @@ const Footer = () => {
   return (
     <footer className="footer-container">
       <div className="contact-column">
-        <h2>Me Contacter</h2>
+        <h2><Link to="/contact">Contactez-moi</Link></h2>
         <p>Pour toute question, collaboration ou simplement pour discuter, n'hésitez pas à me contacter.</p>
-        <Link to="/contact">Contactez-moi</Link>
-        <p>Coordonnées : Tel, Email</p>
+        <p>Coordonnées :</p>
+        <p>06.43.77.55.69</p>
+        <p>r.philippot@live.fr</p>
       </div>
       <div className="legal-column">
         <Link to="/LegalMentions">Mentions Légales</Link>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,25 +1,52 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import MenuDropdown from '../Header/MenuDropDown';
 import '../../styles/Header.css';
 
 const Header = () => {
   const [isMenuOpen, setMenuOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
 
   const toggleMenu = () => {
     setMenuOpen(!isMenuOpen);
   };
 
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <div className="header-container">
       <div className="header-title">
-        <h1><Link to="/">Philippot Raphael Portfolio</Link></h1>
+        <h1><Link to="/"><span className='InitialGold'>N</span>onnorso</Link></h1>
       </div>
-      <div className={`menu-icon ${isMenuOpen ? 'open' : ''}`} onClick={toggleMenu}>
-        ☰
-      </div>
+      {isMobile ? null : (
+        <div className='DesktopNav'>
+          <ul>
+            <li><Link to="/">Accueil</Link></li>
+            <li><Link to="/about">À propos</Link></li>
+            <li><Link to="/portfolios">Portfolios</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+          </ul>
+        </div>
+      )}
 
-      <MenuDropdown isOpen={isMenuOpen} />
+      {isMobile ? (
+              <div className={`menu-icon ${isMenuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+                ☰
+              </div>
+            ) : (
+              null
+            )}
+      
+      {isMobile ? <MenuDropdown isOpen={isMenuOpen} /> : null}
     </div>
   );
 };
