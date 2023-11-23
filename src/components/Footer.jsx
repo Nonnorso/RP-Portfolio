d6500bec5bb6b362cce03bf63d7b31d493ef9172
Nonnorso <r.philippot@live.fr>
2023-11-23 11:04:58 +0100

    ajout de la redirection en debut d epage lors de la navigation depuis le footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import '../styles/Footer.scss';
 
 const Footer = () => {
+
+  const navigate = useNavigate();
+
+  const handleNavigate = (path) => {
+    window.scrollTo(0, 0);
+    navigate(path);
+  };
+
   return (
     <footer className="footer-container">
         <h2 id="contact">Contactez-moi</h2>
         <p><a href="tel:+0643775569"><span className="icon">✆</span> 06.43.77.55.69</a></p>
         <p> <a href="mailto:r.philippot@live.fr"><span className="icon">✉</span> r.philippot@live.fr</a></p>
-        <p><Link to="/LegalMentions">Mentions Légales</Link></p>
+        <p><Link to="/LegalMentions" onClick={() => handleNavigate('/LegalMentions')}>Mentions Légales</Link></p>
         <p>© 2023 Philippot Raphaël All rights reserved</p>
     </footer>
   );
