e99c44bdf91c3e23f96e043591933c907faa7572
Nonnorso <r.philippot@live.fr>
2023-11-23 13:10:08 +0100

    creation du json pour les datas

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,11 +1,9 @@
-import Banner from "../components/Banner";
 import Project from "../components/Projetct";
 import PreviewProject from "../components/PreviewProject";
 
 export default function Projects (){
     return ( 
         <div>
-          <Banner />
           <Project />
           <PreviewProject />
         </div>
