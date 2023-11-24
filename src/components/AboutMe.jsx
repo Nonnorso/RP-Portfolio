585c3b015a97ae7b83f0a8c29bf3a5bb692b774b
Nonnorso <r.philippot@live.fr>
2023-11-24 11:34:52 +0100

    ebauche de style AboutMe

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,9 +1,22 @@
-export default function AboutMe () {
-     return (
-        <div>
-            <img src="" alt=" " title="photo de moi" />
-            <h2>Mon Nom</h2>
-            <p>description</p>
+import '../styles/AboutMe.scss'
+
+export default function AboutMe() {
+    return (
+      <div className="profile-container">
+        <img
+        className="profile-pic"
+          src="assets\Presentation\Photo-descripton.webp"
+          alt="Raphaël Philippot"
+          title="Raphaël Philippot"
+        />
+        <div className='profile-text'>
+            <h2><span className="profile-initial">R</span>aphaël Philippot</h2>
+            <p className="profile-description">
+            Après plusieurs années en tant que vendeur, j'ai découvert ma passion
+            pour le développement web et j'ai entrepris une reconversion à travers
+            une formation complète.
+            </p>
         </div>
-     )
-}
+      </div>
+    );
+  }
