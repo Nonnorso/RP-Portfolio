52f7acda3f61b58bdb6e8ebea65089ca266ddd0e
Nonnorso <r.philippot@live.fr>
2023-11-29 14:26:18 +0100

    changement de numero de contact

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,7 +14,7 @@ const Footer = () => {
   return (
     <footer className="footer-container">
         <h2 id="contact">Contactez-moi</h2>
-        <p><a href="tel:+0643775569"><span className="icon">✆</span> 06.43.77.55.69</a></p>
+        <p><a href="tel:+33643775569"><span className="icon">✆</span> 06.43.77.55.69</a></p>
         <p> <a href="mailto:r.philippot@live.fr"><span className="icon">✉</span> r.philippot@live.fr</a></p>
         <p><Link to="/LegalMentions" onClick={() => handleNavigate('/LegalMentions')}>Mentions Légales</Link></p>
         <p>© 2023 Philippot Raphaël All rights reserved</p>
